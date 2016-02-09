faad6984042272434f4ccc1e7600639f697e892e
Craig Carlson <cjcarlson20@gmail.com>
2016-02-09 00:18:01 -0500

    basic dialogue system working -- uses mecanim state machine

---
+++ OldPeopleInSpace/Assets/Scripts/Dialogue/DialogueState.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogueState : StateMachineBehaviour {
+
+	public string dialogueText;
+	public string[] responses;
+
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		//called on the first frame of the state being played
+
+		//display options on UI
+		DialogueControl.Instance.setDialogue(this, animator);
+	}
+
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+
+		//called after MonoBehaviour Updates on every frame whilst the animator is playing the state this behaviour belongs to
+	}
+
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+
+		//called on the last frame of a transition to another state.
+
+		//reset options
+		animator.SetInteger("optionselect", 0);
+	}
+}
