bb31748561ba2359dfe60b874f333522c4e94c57
John Conover <111jbcon@gmail.com>
2016-02-15 16:42:02 -0500

    Added follower script

---
+++ OldPeopleInSpace/Assets/Scripts/AI/Follower.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follower : MonoBehaviour {
+
+    public float stoppingRadius = 5f;
+
+    NavMeshAgent agent;
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+    // Update is called once per frame
+    void Update() {
+        agent.SetDestination(player.transform.position);
+        if ((transform.position - player.transform.position).magnitude < stoppingRadius) {
+            agent.Stop();
+        }
+        else
+        {
+            agent.Resume();
+        }
+    }
+}
