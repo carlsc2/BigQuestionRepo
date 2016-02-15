356267a94e8ec9d761076b3792c400b434edf476
John Conover <111jbcon@gmail.com>
2016-02-15 15:32:56 -0500

    Added elevator prefab

---
+++ OldPeopleInSpace/Assets/Scripts/Elevator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Elevator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    void OnTriggerEnter(Collider c)
+    {
+
+    }
+
+    public void LoadRoom(string sceneName)
+    {
+        SceneManager.LoadSceneAsync("Scenes/" + sceneName);
+    }
+}
